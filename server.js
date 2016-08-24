81f0aa4ba8757ac660b1244a3f08725a17b180c0
vikas satpute <vikas satpute>
2016-08-24 15:45:31 +0530

    make it better

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ var port = process.env.OPENSHIFT_NODEJS_PORT || 5000
 , ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 app.listen(port, ip, function() {
   console.log('Express server listening on %d', port);
+  console.log('Express server listening on %d', ip);
 });
 
 
